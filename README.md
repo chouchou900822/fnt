b3d33d5a550ddac6f20edaf51f4260db6dcf8346
edison <edison900822@gmail.com>
2018-05-08 16:18:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# fnt
+
+## 安装
+
+### [富能通服务平台](http://service.funenc.com/)
+
+```sh
+# 选择完免登授权类型之后填写对应的参数
+# 参数在富能通服务平台上
+$ npm install fnt -g
+```
+
+![avatar](http://onx1jjc4v.bkt.clouddn.com/WX20180508-161007@2x.png)
+
+## 运行
+```sh
+$ fnt project-name
+$ cd project/ && npm install && npm run dev
+$ cd client && npm install && npm run dev
+```
+
+## 代码贡献流程
+
+1. Fork这个项目
+2. 创建你的feature分支 (`git checkout -b my-new-feature`)
+3. Commit更改 (`git commit -am 'Add some feature'`)
+4. Push到这个分支 (`git push origin my-new-feature`)
+5. 新建Pull Request
---
+++ index.js
@@ -0,0 +1,134 @@
+#!/usr/bin/env node
+const prompts = require('prompts');
+const exec = require('child_process').exec;
+const request = require('request');
+const fs = require('fs');
+const colors = require('colors');
+const os = require('os');
+const download = require('download-git-repo');
+const spin = require('io-spin')
+
+const source = 'https://raw.githubusercontent.com/chouchou900822/template/master';
+
+function getIPAdress() {
+  var interfaces = os.networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+}
+
+async function downloadFile(folder) {
+  return new Promise(function (resolve, reject) {
+    download(`chouchou900822/enterprise`, `./${folder}`, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    })
+  });
+}
+async function downloadSingleFile(uri, filename) {
+  return new Promise(function (resolve) {
+    let stream = fs.createWriteStream(filename);
+    request(source + uri).pipe(stream).on('close', resolve());
+  });
+}
+
+async function get(url) {
+  let host = 'http://service.funenc.com/api'
+  return new Promise(function (resolve, reject) {
+    request(encodeURI(host + url), function (error, response, body) {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(JSON.parse(response.body));
+      }
+    });
+  });
+}
+(async function () {
+  let folders = [{
+    type: 'text',
+    name: 'folder',
+    message: `项目名称`
+  }];
+
+  const folderResponse = await prompts(folders);
+
+  let questions = [{
+    message: '选择你的项目类型',
+    type: 'select',
+    name: 'corp',
+    choices: [
+      { title: '企业微信', value: 'wx' },
+      { title: '钉钉', value: 'dd' }
+    ],
+    initial: 0
+  }, {
+    message: '选择你的免登类型',
+    type: 'select',
+    name: 'login',
+    choices: [
+      { title: '网页授权', value: 'web' },
+      { title: '扫码授权', value: 'scan' }
+    ],
+    initial: 0
+  }];
+  const response = await prompts(questions);
+
+  let inputs = [{
+    type: 'text',
+    name: 'userid',
+    message: `请输入userid`
+  }, {
+    type: 'text',
+    name: 'appName',
+    message: `请输入appName`
+  }];
+
+  const envResponse = await prompts(inputs);
+  
+  let spinner = spin('代码拉取中...'.green);
+  spinner.start();
+  await downloadFile(folderResponse.folder);
+  spinner.update('80%...');
+  await downloadSingleFile(`/${response.corp}/${response.login}/App.vue?v=${Math.random()}`, `./${folderResponse.folder}/client/src/App.vue`);
+  spinner.update('85%...');
+  await downloadSingleFile(`/${response.corp}/${response.login}/funenc.js?v=${Math.random()}`, `./${folderResponse.folder}/controllers/funenc.js`);
+  spinner.update('90%...');
+  await downloadSingleFile(`/${response.corp}/${response.login}/index.html?v=${Math.random()}`, `./${folderResponse.folder}/client/public/index.html`);
+  spinner.update('95%...');
+  await downloadSingleFile(`/${response.corp}/${response.login}/index.js?v=${Math.random()}`, `./${folderResponse.folder}/routes/index.js`);
+  spinner.update('100%...');
+  spinner.stop();
+  console.log('代码下载成功...'.green);
+
+  let envSpinner = spin('环境变量配置中...'.green);
+  envSpinner.start();
+  let ip = getIPAdress();
+  envResponse.host = `http://${ip}:3000`;
+  let serviceRes = await get(`/admins/info?corp=${response.corp}&login=${response.login}&userid=${envResponse.userid}&appName=${envResponse.appName}`);
+  let envFileContent = '';
+  for (let key in envResponse) {
+    envFileContent += `VUE_APP_${key}=${envResponse[key]}\n`;
+  }
+  for (let key in serviceRes) {
+    envFileContent += `VUE_APP_${key}=${serviceRes[key]}\n`;
+  }
+  exec(`echo "${envFileContent}" > ./${folderResponse.folder}/.env.development`, function (error, stdout, stderr) {
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    } else {
+      envSpinner.stop();
+      console.log('环境变量配置完成...'.green);
+      console.log('项目创建完毕...'.green);
+    }
+  });
+})();
